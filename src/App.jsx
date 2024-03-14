4dfe6aaae1b5a44d3c60ba19062b4a7fd1563741
foy4748 <faisaljfcl@gmail.com>
2024-03-14 16:08:54 +0600

    Removed import 'animate.css' since tailwind is handling it

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import "animate.css";
+//import "animate.css";
 import { useRef } from "react";
 
 function App() {
